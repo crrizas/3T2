63e72a5d44b2b031bba068a9fe154e9ad818586c
crrizas <144707678+crrizas@users.noreply.github.com>
2025-05-16 10:12:23 -0300

    Add files via upload

--- 3T2-main/crrizos/main.js
+++ 3T2-main/crrizos/main.js
@@ -4,7 +4,7 @@ for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function(){
 
         for(let j=0;k<botoes.length;j++){
-            botoes[]
+            botoes[j].classList.remove("ativo");
         }
 
         botoes[i].classList.add("ativo");
